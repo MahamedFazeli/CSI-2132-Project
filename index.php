27248970c9638aa99f32af630c519dbc4ad59861
mahamedfazeli <mfazeli125@gmail.com>
2023-04-09 01:50:00 -0400

    updated again

--- index.php
+++ index.php
@@ -18,6 +18,18 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $price = $_POST['price'];
     $query .= " AND price <= $price";
   }
+   if (!empty($_POST['number_of_rooms'])) {
+    $num_rooms = $_POST['num-rooms'];
+    $query .= " AND hotel_ID IN (SELECT hotel_ID FROM hotel WHERE number_of_rooms <= $num_rooms)";
+  }
+  if (!empty($_POST['hotel-chain'])) {
+    $hotel_chain = $_POST['hotel-chain'];
+    $query .= " AND hotel_ID IN (SELECT hotel_id FROM hotel WHERE Chain_ID = '$hotel_chain')";
+  }
+   if (!empty($_POST['hotel'])) {
+    $hotel = $_POST['hotel'];
+    $query .= " AND hotel_ID = '$hotel'";
+  }
 
   // Execute the query
   $result = mysqli_query($con, $query);
@@ -27,7 +39,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     echo "<p>No results found.</p>";
   } else {
     echo "<table>";
-    echo "<tr><th>Room Number</th><th>Hotel ID</th><th>Capacity</th><th>Price</th></tr>";
+    echo "</th><th>Hotel ID</th><th>Capacity</th><th>Price</th></tr>";
     while ($row = mysqli_fetch_assoc($result)) {
       echo "</td><td>" . $row['hotel_ID'] . "</td><td>" . $row['Capacity'] . "</td><td>" . $row['Price'] . "</td></tr>";
     }
@@ -60,13 +72,34 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
             <option value="5">5 people</option>
             <option value="6">6 people</option>
         </select>
-
-        <br>
+        <label for="num-rooms">Number of rooms in hotel:</label>
+        <input type="number" id="num-rooms" name="num-rooms" min="0">
+        
+        <label for="hotel-chain">Hotel chain:</label>
+        <select id="hotel-chain" name="hotel-chain">
+            <option value="">Any</option>
+            <option value="chain1">Chain 1</option>
+            <option value="chain2">Chain 2</option>
+            <option value="chain3">Chain 3</option>
+            <option value="chain4">Chain 4</option>
+            <option value="chain5">Chain 5</option>
+        </select>
 
         <label for="price">Price:</label>
         <input type="number" id="price" name="price">
 
-        <br>
+        <label for="hotel">Hotel:</label>
+        <select id="hotel" name="hotel">
+            <option value="">Any</option>
+            <option value="hotel1">Hotel 1</option>
+            <option value="hotel2">Hotel 2</option>
+            <option value="hotel3">Hotel 3</option>
+            <option value="hotel4">Hotel 4</option>
+            <option value="hotel5">Hotel 5</option>
+            <option value="hotel6">Hotel 6</option>
+            <option value="hotel7">Hotel 7</option>
+            <option value="hotel8">Hotel 8</option>
+        </select>
 
         <button type="submit">Search</button>
     </form>
