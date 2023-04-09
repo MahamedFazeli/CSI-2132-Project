bd825a62d8e6c77184684acdec6f8d567c4320e5
tahahoussari <tahahoussari@gmail.com>
2023-04-09 14:41:41 -0400

    Added buttons to employee page
    -buttons lead to other pages where employee can make changes

---
+++ customerRenting.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Login page</title>
+    </head>
+
+    <body>
+        <style>
+
+        </style>
+
+        <div id="box">
+            <h1>Customer Renting Page</h1>
+        </div>
+    </body>
+</html>
--- employeePage.php
+++ employeePage.php
@@ -3,6 +3,8 @@ session_start();
 
 include("connection.php");
 include("functions.php");
+
+
 ?>
 
 
@@ -11,7 +13,34 @@ include("functions.php");
 <head>
     <title>Employee Page</title>
 </head>
+<style>
+    .content{
+        max-width: 500px;
+        margin: auto;
+        text-align: center;
+    }
+    .title{
+        text-align: center;
+    }
+    
+    .btn{
+        padding: 5px;
+        margin-left: 10px;
+    }
+    .link{
+        text-decoration: none;
+        color: black;
+    }
+</style>
 <body>
-    <h1>E-Hotels Employee Page</h1>
+    <div class="content">
+        <h1 class="title">E-Hotels Employee Page</h1><br><br>
+        <button class="btn"><a class ="link" href="adding.php">Add Hotel/Room</button>
+        <button class="btn"><a class ="link" href="removing.php">Remove Hotel/Room</button>
+        <button class="btn"><a class ="link" href="modifying.php">Modify Hotel/Room</button><br><br>
+
+        <button class="btn"><a class ="link" href="customerBooking.php">Customer Booking</button>
+        <button class="btn"><a class ="link" href="customerRenting.php">Customer Renting</button>
+    </div>
 </body>
 </html>
