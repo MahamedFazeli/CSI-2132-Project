cb871779dc085c68ce7a913924944c297321681b
tahahoussari <tahahoussari@gmail.com>
2023-04-10 23:18:22 -0400

    small modification in booking page

--- customerBooking.php
+++ customerBooking.php
@@ -1,36 +1,3 @@
-<?php
-    session_start();
-
-    include("connection.php");
-    include("functions.php");
-    if($_SERVER['REQUEST_METHOD'] == "POST"){
-        //something was posted
-        $renting_ID = $_POST['renting_ID'];
-        $checkInDate = $_POST['check_in_date'];
-        $price = $_POST['price'];
-        $durationOfRent = $_POST['duration_of_rent'];
-        $room_ID = $_POST['room_ID'];
-        $payment_ID = $_POST['payment_ID'];
-        $booking_ID = $_POST['booking_ID'];
-        $hotel_ID = $_POST['hotel_ID'];
-
-        if(empty($renting_ID) || empty($checkInDate) || empty($durationOfRent) || empty($room_ID) || empty($payment_ID) || empty($booking_ID) || empty($hotel_ID)){
-            echo("Please don't leave anything blank!!!");
-        }else{
-            $query = "insert into renting (renting_ID , check_in_date, price, duration_of_rent, room_ID, payment_ID, booking_ID, hotel_ID) values ('$renting_ID','$checkInDate', '$price', '$durationOfRent', '$room_ID', '$payment_ID','$booking_ID', '$hotel_ID')";
-            mysqli_query($con, $query);
-
-            $query2 = "delete from booking where booking_ID = ('$booking_ID')";
-            mysqli_query($con, $query2);
-
-            Header("Location: successPage.php");
-            die;
-        }
-    }
-
-?>
-
-
 <!DOCTYPE html>
 <html>
     
@@ -41,9 +8,6 @@
         <style>
             .content{
                 text-align: center;
-            }
-            .firstForm{
-                float: left;
             }
             .link{
                 text-decoration: none;
@@ -59,7 +23,9 @@
             <div class="inputs">
                 <h2>Booking ID</h2>
                 <form class="forms" method="post">
-                    <input class="box" id="text" type="text" name="booking_ID" placeholder="Booking ID">
+                    <input type="hidden" id="text" name="book"  value="0">
+                    <input type="checkbox" id="text" name="book" value="1">
+                    <input class="box" id="text" type="text" name="booking_ID2" placeholder="Booking ID">
                     <input class="submit" id="button" type="submit" value="Submit"><br><br>
             </div>
 
@@ -77,6 +43,51 @@
                     Hotel ID:<input class="box" id="text" type="text" name="hotel_ID" placeholder="Hotel ID"><br><br>
                     <input class="submit" id="button" type="submit" value="Submit">
                 </form>
-            </div>
+            </div><br><br>
+            
+            <?php
+                session_start();
+
+                include("connection.php");
+                include("functions.php");
+                if($_SERVER['REQUEST_METHOD'] == "POST"){
+                    //something was posted
+                    $booking_ID2 = $_POST['booking_ID2'];
+                    $book = $_POST['book'];
+
+                    $renting_ID = $_POST['renting_ID'];
+                    $checkInDate = $_POST['check_in_date'];
+                    $price = $_POST['price'];
+                    $durationOfRent = $_POST['duration_of_rent'];
+                    $room_ID = $_POST['room_ID'];
+                    $payment_ID = $_POST['payment_ID'];
+                    $booking_ID = $_POST['booking_ID'];
+                    $hotel_ID = $_POST['hotel_ID'];
+                    if(empty($booking_ID2) && $book == 1){
+                        echo("Please don't booking ID blank.");
+                    }elseif($book == 1){
+                        $query3 = "select * from booking where booking_ID = ('$booking_ID2')";
+                        $result = mysqli_query($con, $query3);
+                        $row = mysqli_fetch_array($result);
+                        print "<pre>";
+                        print_r($row);
+                        print "</pre>";
+                    }
+
+                    if((empty($renting_ID) || empty($checkInDate) || empty($durationOfRent) || empty($room_ID) || empty($payment_ID) || empty($booking_ID) || empty($hotel_ID))&& $book == 0){
+                        echo("Please don't leave anything blank!!!");
+                    }elseif($book == 0){
+                        $query = "insert into renting (renting_ID , check_in_date, price, duration_of_rent, room_ID, payment_ID, booking_ID, hotel_ID) values ('$renting_ID','$checkInDate', '$price', '$durationOfRent', '$room_ID', '$payment_ID','$booking_ID', '$hotel_ID')";
+                        mysqli_query($con, $query);
+
+                        $query2 = "delete from booking where booking_ID = ('$booking_ID')";
+                        mysqli_query($con, $query2);
+
+                        Header("Location: successPage.php");
+                        die;
+                    }
+                }
+
+            ?>
     </body>
 </html>
--- customerPayment.php
+++ customerPayment.php
@@ -1,25 +1,4 @@
-<?php
-    session_start();
 
-    include("connection.php");
-    include("functions.php");
-    if($_SERVER['REQUEST_METHOD'] == "POST"){
-        //something was posted
-        $payment_ID = $_POST['payment_ID'];
-        $cardNumber = $_POST['card_number'];
-        $cardName = $_POST['card_name'];
-        $CVV = $_POST['CVV'];
-        $customer_SSN = $_POST['customer_SSN'];
-
-        if(empty($payment_ID) || empty($cardNumber) || empty($cardName) || empty($CVV) || empty($customer_SSN)){
-            echo("Please don't leave anything blank!!!");
-        }else{
-            $query = "insert into payment (payment_ID , card_number, card_name, CVV, customer_SSN) values ('$payment_ID','$cardNumber', '$cardName', '$CVV', '$customer_SSN')";
-            $result = mysqli_query($con, $query);
-        }
-    }
-
-?>
 
 
 <!DOCTYPE html>
@@ -57,7 +36,31 @@
                     <input class="submit" id="button" type="submit" value="Submit">
                     <button><a class="link" href="customerBooking.php">Booking</a></button>
                 </form>
-                <?php echo("Payment ID: ".$payment_ID);?>
+                <?php
+    session_start();
+
+    include("connection.php");
+    include("functions.php");
+    if($_SERVER['REQUEST_METHOD'] == "POST"){
+        //something was posted
+        $payment_ID = null;
+        $payment_ID = $_POST['payment_ID'];
+        $cardNumber = $_POST['card_number'];
+        $cardName = $_POST['card_name'];
+        $CVV = $_POST['CVV'];
+        $customer_SSN = $_POST['customer_SSN'];
+
+        if(empty($payment_ID) || empty($cardNumber) || empty($cardName) || empty($CVV) || empty($customer_SSN)){
+            echo("Please don't leave anything blank!!!");
+        }else{
+            $query = "insert into payment (payment_ID , card_number, card_name, CVV, customer_SSN) values ('$payment_ID','$cardNumber', '$cardName', '$CVV', '$customer_SSN')";
+            $result = mysqli_query($con, $query);
+
+            echo("Payment ID: ".$payment_ID);
+        }
+    }
+
+?>
             </div>
     </body>
 </html>
