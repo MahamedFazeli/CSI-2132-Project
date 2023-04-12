93d913b3ed923c8051224483ceccc72d163bcb8a
mahamedfazeli <mfazeli125@gmail.com>
2023-04-12 03:54:58 -0400

    Search works now

--- index.php
+++ index.php
@@ -37,30 +37,13 @@ $user_data = check_login($con);
         <input type="number" id="num-rooms" name="num-rooms" min="0">
         
         <label for="hotel-chain">Hotel chain:</label>
-        <select id="hotel-chain" name="hotel-chain">
-            <option value="">Any</option>
-            <option value="chain1">Chain 1</option>
-            <option value="chain2">Chain 2</option>
-            <option value="chain3">Chain 3</option>
-            <option value="chain4">Chain 4</option>
-            <option value="chain5">Chain 5</option>
-        </select>
+        <input type="text" id="hotel-chain" name="hotel-chain">
 
         <label for="price">Price:</label>
         <input type="number" id="price" name="price">
 
         <label for="hotel">Hotel:</label>
-        <select id="hotel" name="hotel">
-            <option value="">Any</option>
-            <option value="hotel1">Hotel 1</option>
-            <option value="hotel2">Hotel 2</option>
-            <option value="hotel3">Hotel 3</option>
-            <option value="hotel4">Hotel 4</option>
-            <option value="hotel5">Hotel 5</option>
-            <option value="hotel6">Hotel 6</option>
-            <option value="hotel7">Hotel 7</option>
-            <option value="hotel8">Hotel 8</option>
-        </select>
+        <input type="text" id="hotel" name="hotel">
         <label for="duration_of_stay">Duration of Stay (in days):</label>
         <input type="number" id="duration_of_stay" name="duration_of_stay" min="1">
         <button type="submit">Search</button>
@@ -112,9 +95,9 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     echo "<p>No results found.</p>";
   } else {
     echo "<table>";
-    echo "<tr><th>Hotel ID</th><th>Room number</th><th>Capacity</th><th>Price</th><th>Book</th></tr>";
+    echo "<tr><th>Hotel ID</th><th>Room number</th><th>Capacity</th><th>Price</th><th>View</th><th>Book</th></tr>";
     while ($row = mysqli_fetch_assoc($result)) {
-      echo "<tr><td>" . $row['hotel_ID'] . "</td><td>" . $row['room_ID'] . "</td><td>" . $row['Capacity'] . "</td><td>" . $row['Price'] . "</td><td><form method='POST'><input type='hidden' name='room_id' value='" . $row['room_ID'] . "'><button type='submit'>Book</button></form></td></tr>";
+      echo "<tr><td>" . $row['hotel_ID'] . "</td><td>" . $row['room_ID'] . "</td><td>" . $row['Capacity'] . "</td><td>" . $row['Price'] ."</td><td>" . $row['view_type'] . "</td><td><form method='POST'><input type='hidden' name='room_id' value='" . $row['room_ID'] . "'><button type='submit'>Book</button></form></td></tr>";
     }
     echo "</table>";
   }
